6904f03ba1adc7a44d420bdd8072bed6ee1d6c0d
Lucas Chanet <lucaschanet@gmail.com>
2019-11-15 11:41:19 +0000

    Add Article scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     resources :cocktails do
       resources :doses, only: [:new, :create]
     end
   resources :doses, only: [:destroy]
+  root to: 'articles#index'
+  resources :articles, except: :index
 end
